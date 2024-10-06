a265ac4ba193b7e5001a5af4b7075c544e763221
riduuu <faridhossainseoprovider01@gmail.com>
2024-10-06 20:34:03 +0300

    add

---
+++ main.js
@@ -0,0 +1,148 @@
+
+
+
+let mathans1 = 'math_01_answer_is'
+console.log(mathans1);
+// math 01 
+let sarar_apple_chilo  = 25;
+let she_apple_kinlo    = 15;
+
+let total_apples = ( sarar_apple_chilo + she_apple_kinlo );
+
+console.log(total_apples);
+// math 01 
+
+
+
+
+
+let mathans2 = 'math_02_answer_is'
+console.log(mathans2);
+// math 02 
+
+let john_er_taka_chilo = 500;
+let dokane_taka_dilo =200;
+
+let john_er_kache_taka_roilo = ( john_er_taka_chilo -
+ dokane_taka_dilo);
+
+console.log(john_er_kache_taka_roilo);
+// math 02 
+
+
+
+
+
+
+let mathans3 = 'math_03_answer_is'
+console.log(mathans3);
+// math 03
+
+let per_hour_machine_hoy = 120;
+let total_hours = 8;
+
+let eight_hour_a_machine_hobe = ( per_hour_machine_hoy * total_hours );
+
+console.log( eight_hour_a_machine_hobe );
+// math 03 
+
+
+
+
+
+
+let mathans4 = 'math_04_answer_is'
+console.log(mathans4);
+// math 04 
+
+let per_line_gach_ache = 12;
+let total_line_ache = 8;
+
+let shob_line_a_gach_ache = ( per_line_gach_ache * total_line_ache);
+
+let shob_line_a_total_gach_ache = (shob_line_a_gach_ache);
+let gach_add_kora_hoyeche = 20;
+
+let bagane_total_gach_hobe = (shob_line_a_gach_ache + gach_add_kora_hoyeche );
+
+console.log( bagane_total_gach_hobe );
+// math 04 
+
+
+
+
+
+
+let mathans5 = 'math_05_answer_is'
+console.log(mathans5);
+// math 05
+
+let per_box_komola_ache = 10;
+let total_box_ache = 5;
+
+let total_komola = (per_box_komola_ache * total_box_ache);
+
+let total_komola_chilo = ( total_komola);
+let friend_ke_diyeche = 12;
+
+let komola_baki_roilo = ( total_komola - friend_ke_diyeche );
+
+console.log( komola_baki_roilo );
+// math 05
+
+
+
+
+
+
+let mathans6 = 'math_06_answer_is'
+console.log(mathans6);
+// math 06 
+let train_chole_kilometre = 150;
+let time_lage_hour = 3;
+
+let per_hour_train_jay = ( train_chole_kilometre / time_lage_hour );
+
+console.log( per_hour_train_jay);
+// math 06 
+
+
+
+
+
+let mathans7 = 'math_07_answer_is'
+console.log(mathans7);
+// math 07
+
+let product_quantity = 200;
+let machine_quantity = 6;
+
+let per_machine_product = parseInt( product_quantity / machine_quantity);
+
+let machine_chilo = 6;
+let machine_add_hoyeche = 2;
+let total_machien = ( machine_chilo + machine_add_hoyeche);
+let total_product_quantity = (per_machine_product * total_machien);
+
+console.log( total_product_quantity );
+// total_product_quantity 264
+let product_sell_hoy_ni = 3;
+let $3_prodict_sell_na_hole_total_product_hobe = (total_product_quantity - product_sell_hoy_ni );
+
+console.log($3_prodict_sell_na_hole_total_product_hobe);
+
+// math 07
+
+
+
+
+let mathans8 = 'math_08_answer_is'
+console.log(mathans8);
+
+// math 08
+let total_chocolate = 120;
+let per_packet_chocolate = 9;
+let extra_chocolate = ( total_chocolate  % per_packet_chocolate );
+
+console.log( extra_chocolate );
+// math 08
